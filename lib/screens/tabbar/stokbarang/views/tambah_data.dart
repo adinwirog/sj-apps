812e3daeceb97a926da695dd2a19b2b4ae1f3e0f
adinwirog <adinexion@gmail.com>
2021-11-04 22:57:39 +0700

    adding tambah data screen

--- lib/screens/tabbar/stokbarang/views/tambah_data.dart
+++ lib/screens/tabbar/stokbarang/views/tambah_data.dart
@@ -12,7 +12,66 @@ class _TambahDataViewState extends State<TambahDataView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffFCEDEA),
-      body: Center(child: Text("Tambah Data")),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: "Cari Barang...",
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+              keyboardType: TextInputType.text,
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) => Card(
+                  child: ListTile(
+                    title: Text(
+                      "Barang $index",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    trailing: Wrap(
+                      spacing: 25,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.edit, color: Colors.black),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.delete, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xff7165FF),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
