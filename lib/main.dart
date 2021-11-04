5276f91d80c0c0997b9228e132168a35382c95e5
adinwirog <adinexion@gmail.com>
2021-11-05 05:27:05 +0700

    detail barang screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sarangjembar_apps/screens/detail_barang.dart';
 import 'package:sarangjembar_apps/screens/main_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class CoreApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+      home: DetailBarangScreen(),
     );
   }
 }
